c79dc5b02ef0db70f58656a085b148139896b40b
katsube-toshiki <katsube@mi.t.u-tokyo.ac.jp>
2024-12-03 18:20:28 +0900

    error修正

--- scripts/demo/demo_llava.sh
+++ scripts/demo/demo_llava.sh
@@ -9,6 +9,8 @@ module load cudnn/8.8.1
 
 pyenv shell 3.10.15
 source /work/gn53/k75057/musasabi/bin/activate
+export PYTHONPATH=$PYTHONPATH:/work/gn53/k75057/projects/LLaVA-JP
+which python
 
 readonly LLAVA_JP_HOME="/work/gn53/k75057/projects/LLaVA-JP"
 readonly COMMONCRAWL_HOME="/work/gn53/k75057/projects/commoncrawl"
