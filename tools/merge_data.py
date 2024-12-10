c4581204ccdc49991a6072f1714fb0a1c07373df
katsube-toshiki <katsube@mi.t.u-tokyo.ac.jp>
2024-12-10 17:16:22 +0900

    merge data

--- tools/merge_data.py
+++ tools/merge_data.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 if __name__ == '__main__':
     base_path = Path("/work/gn53/k75057/projects")
+    new_base_path = Path("/work/gn53/k75057/projects/LLaVA-JP/dataset")
 
     marge_json_path1 = Path(base_path, "commoncrawl/data/output_json", 'llava_pretrain_1022k.json')
     marge_data_path1 = Path(base_path, "commoncrawl/data/images")
@@ -14,14 +15,19 @@ if __name__ == '__main__':
 
     with marge_json_path1.open('r', encoding='utf-8') as f:
         caption_data = f.read()
-        caption_data_json = json.loads(caption_data)
-        print(caption_data_json)
+        caption_data_json1 = json.loads(caption_data)
+        for i in range(len(caption_data_json1)):
+            caption_data_json1[i]['image'] = Path(marge_data_path1, caption_data_json1[i]["image"]).relative_to(new_base_path)
 
     with marge_json_path2.open('r', encoding='utf-8') as f:
         caption_data = f.read()
-        caption_data_json.extend(json.loads(caption_data))
+        caption_data_json2 = json.loads(caption_data)
+        for i in range(len(caption_data_json2)):
+            caption_data_json2[i]['image'] = Path(marge_data_path2, caption_data_json2[i]["image"]).relative_to(new_base_path)
 
-    chat_ja_path = Path('llava_pretrain_stair.json')
+    caption_data_json = caption_data_json1 + caption_data_json2
+
+    chat_ja_path = Path(new_base_path, 'laioncc_w_1022k.json')
     with open(chat_ja_path, mode="w") as f:
         json.dump(caption_data_json, f, indent=2, ensure_ascii=False)
     
