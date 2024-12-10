e2a0394c7f14b0376e4c95b6418d95ee621ee19b
katsube-toshiki <katsube@mi.t.u-tokyo.ac.jp>
2024-12-10 17:05:18 +0900

    merge data

--- tools/merge_data.py
+++ tools/merge_data.py
@@ -4,19 +4,24 @@ import json
 from pathlib import Path
 
 if __name__ == '__main__':
-    dataset_dir = './'
-    marge_path1 = Path(dataset_dir, "LLaVA-CC3M-Pretrain-595K", 'chat_ja_calm2.json')
-    marge_path2 = Path(dataset_dir, "LLaVA-Stair-Caption", 'llava_stair_caption.json')
+    base_path = Path("/work/gn53/k75057/projects")
 
-    with marge_path1.open('r', encoding='utf-8') as f:
+    marge_json_path1 = Path(base_path, "commoncrawl/data/output_json", 'llava_pretrain_1022k.json')
+    marge_data_path1 = Path(base_path, "commoncrawl/data/images")
+
+    marge_json_path2 = Path(base_path, "LLaVA-JP/dataset", 'llava_pretrain_blip_laion_cc_sbu_558k_ja.json')
+    marge_data_path2 = Path(base_path, "LLaVA-JP/dataset/llava-pretrain")
+
+    with marge_json_path1.open('r', encoding='utf-8') as f:
         caption_data = f.read()
         caption_data_json = json.loads(caption_data)
+        print(caption_data_json)
 
-    with marge_path2.open('r', encoding='utf-8') as f:
+    with marge_json_path2.open('r', encoding='utf-8') as f:
         caption_data = f.read()
         caption_data_json.extend(json.loads(caption_data))
 
-    chat_ja_path = Path(dataset_dir, 'llava_pretrain_stair.json')
+    chat_ja_path = Path('llava_pretrain_stair.json')
     with open(chat_ja_path, mode="w") as f:
         json.dump(caption_data_json, f, indent=2, ensure_ascii=False)
     
