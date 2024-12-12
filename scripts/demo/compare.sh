52e99e2e795773539ff29cfbaa087306e5015641
katsube-toshiki <katsube@mi.t.u-tokyo.ac.jp>
2024-12-12 15:12:23 +0900

    定性比較

---
+++ scripts/demo/compare.sh
@@ -0,0 +1,13 @@
+pjsub --interact -g gn53 -L rscgrp=share-interactive
+
+module load cuda/12.1
+module load cudnn/8.8.1
+
+pyenv shell 3.10.15
+source /work/gn53/k75057/musasabi/bin/activate
+export PYTHONPATH=$PYTHONPATH:/work/gn53/k75057/projects/LLaVA-JP
+which python
+
+readonly LLAVA_JP_HOME="/work/gn53/k75057/projects/LLaVA-JP"
+
+python $LLAVA_JP_HOME/src/demo_compare.py
