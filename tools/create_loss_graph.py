0aa49a680415303de479277f632783355c538622
katsube-toshiki <katsube@mi.t.u-tokyo.ac.jp>
2024-12-14 14:15:53 +0900

    finetune

--- tools/create_loss_graph.py
+++ tools/create_loss_graph.py
@@ -5,24 +5,29 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     train_data_paths = [
-        '../output_llava/checkpoints/pretrain-llava-jp-1.3b-v1.1-1022k',
-        '../output_llava/checkpoints/pretrain-llava-jp-1.3b-v1.1-laioncc',
-        '../output_llava/checkpoints/pretrain-llava-jp-1.3b-v1.1-laioncc-w-1022k'
+        {
+            'path': '../output_llava/checkpoints/finetune-llava-jp-1.3b-v1.1-1022k-to-gpt',
+            'label': '1022k-to-gpt',
+        },
+        {
+            'path': '../output_llava/checkpoints/finetune-llava-jp-1.3b-v1.1-1022k-to-1022k',
+            'label': '1022k-to-1022k',
+        },
     ]
 
     train_losses = []
     for data_path in train_data_paths:
-        with open(Path(data_path, 'trainer_state.json')) as f:
+        with open(Path(data_path['path'], 'trainer_state.json')) as f:
             log_data = f.read()
             log_data_json = json.loads(log_data)
 
         train_losses.append([history['loss'] for history in log_data_json['log_history'][:-1]])
 
     for i, train_loss in enumerate(train_losses):
-        plt.plot(train_loss, label=str(i+1), linewidth=0.5)
+        plt.plot(train_loss, label=train_data_paths[i]['label'], linewidth=0.5)
     
     plt.title('Pretrain Loss')
     plt.xlabel('Step')
     plt.ylabel('Loss')
     plt.legend(loc='upper right')
-    plt.savefig("pretrain.png")
+    plt.savefig("finetune_1022k.png")
