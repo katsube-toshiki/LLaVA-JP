ea4a02605879bd545d329c9b4818ef2b749730c1
katsube-toshiki <katsube@mi.t.u-tokyo.ac.jp>
2025-01-04 13:58:53 +0900

    model_eval

---
+++ scripts/demo/model_eval.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#PJM -L rscgrp=share-short
+#PJM -L gpu=1
+#PJM -g gn53
+#PJM -X
+#PJM -j
+module load cuda/12.1
+module load cudnn/8.8.1
+
+pyenv shell 3.10.15
+source /work/gn53/k75057/musasabi/bin/activate
+
+readonly LLAVA_JP_HOME="/work/gn53/k75057/projects/LLaVA-JP"
+cd $LLAVA_JP_HOME
+
+python src/model_eval.py
